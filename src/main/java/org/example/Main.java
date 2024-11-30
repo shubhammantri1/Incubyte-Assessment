8d7e73c0921f34171d16ca86208b2cd291403d74
Shubham Mantri <smantri600@gmail.com>
2024-11-30 16:20:07 +0530

    Task 4: added support for delimiters

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,31 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class Main {
+
+    Map<String, Integer> delimeters = new HashMap<>();
+
+    public Main() {
+
+        //Initialize the delimeters
+        delimeters.put(";", 3);
+        delimeters.put("", 0);
+    }
+
 
     /**
      * Method will take a string of comma or new line separated numbers. It will add all the numbers
      * and return the sum of all the numbers.
      *
+     * Added support for delimeters.
+     *
      * @param numbers
      * @return
      */
-    public static int add(String numbers) throws  NumberFormatException, Exception {
+    public int add(String numbers) throws  NumberFormatException, Exception {
 
         if(Objects.isNull(numbers) || numbers.isEmpty())
             return 0;
@@ -22,7 +36,10 @@ public class Main {
             String[] numberList = numbers.split("[,\\n]+");
             for(String number: numberList) {
                 int num = 0;
-                if(!number.isEmpty()){
+                if(delimeters.containsKey(number)){
+                    num = delimeters.get(number);
+                }
+                else{
                     num = Integer.parseInt(number);
                 }
                 result = num + result;
@@ -43,20 +60,28 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
+        Main addNumbers = new Main();
+
         // Task 1
-        System.out.println(add("")); // Output: 0
-        System.out.println(add("1,5")); // Output: 6
-        System.out.println(add("6")); // Output: 6
+        System.out.println(addNumbers.add("")); // Output: 0
+        System.out.println(addNumbers.add("1,5")); // Output: 6
+        System.out.println(addNumbers.add("6")); // Output: 6
 
         // Task 2
-        System.out.println(add("1,2,3")); // Output: 6
-        System.out.println(add("-1,1,3,5")); // Output: 8
-        System.out.println(add(",,0,8")); // Output: 8
+        System.out.println(addNumbers.add("1,2,3")); // Output: 6
+        System.out.println(addNumbers.add("-1,1,3,5")); // Output: 8
+        System.out.println(addNumbers.add(",,0,8")); // Output: 8
 
         // Task 3
-        System.out.println(add("1,2\n3")); // Output: 6
-        System.out.println(add("1\n-2\n3")); // Output: 2
-        System.out.println(add("\n1,2\n3")); // Output: 6
+        System.out.println(addNumbers.add("1,2\n3")); // Output: 6
+        System.out.println(addNumbers.add("1\n-2\n3")); // Output: 2
+        System.out.println(addNumbers.add("\n1,2\n3")); // Output: 6
+
+        // Task 4
+        System.out.println(addNumbers.add(";,;,;")); // Output: 6
+        System.out.println(addNumbers.add(";")); // Output: 3
+        System.out.println(addNumbers.add(",,0,8")); // Output: 8
+
 
     }
 }
--- src/test/java/AddNumbersTest.java
+++ src/test/java/AddNumbersTest.java
@@ -3,34 +3,43 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class AddNumbersTest {
-
+    
     @Test
     void validNumbersTest() throws Exception {
-        Assertions.assertEquals(100, Main.add("5,95"));
-        Assertions.assertEquals(-2, Main.add("5,-7"));
-        Assertions.assertEquals(0, Main.add("5,-5"));
 
-        Assertions.assertEquals(10, Main.add("1,2,3,4"));
-        Assertions.assertEquals(0, Main.add(",,"));
-        Assertions.assertEquals(2,Main.add(",0,2,,"));
+        Main addNumbers = new Main();
+        
+        Assertions.assertEquals(100, addNumbers.add("5,95"));
+        Assertions.assertEquals(-2, addNumbers.add("5,-7"));
+        Assertions.assertEquals(0, addNumbers.add("5,-5"));
 
-        Assertions.assertEquals(3, Main.add("1\n2,"));
-        Assertions.assertEquals(-1, Main.add("1\n-2,"));
-        Assertions.assertEquals(0, Main.add("\n,"));
+        Assertions.assertEquals(10, addNumbers.add("1,2,3,4"));
+        Assertions.assertEquals(0, addNumbers.add(",,"));
+        Assertions.assertEquals(2,addNumbers.add(",0,2,,"));
+
+        Assertions.assertEquals(3, addNumbers.add("1\n2,"));
+        Assertions.assertEquals(-1, addNumbers.add("1\n-2,"));
+        Assertions.assertEquals(0, addNumbers.add("\n,"));
+
+        Assertions.assertEquals(9, addNumbers.add(";,;\n;"));
+        Assertions.assertEquals(8, addNumbers.add("0,;\n5"));
 
     }
 
     @Test
     void edgeCasesNumbersTest() {
-        Assertions.assertThrows(NumberFormatException.class, () -> Main.add(" ,7"));
-        Assertions.assertThrows(NumberFormatException.class, () -> Main.add(" , "));
-        Assertions.assertThrows(NumberFormatException.class, () -> Main.add("asd,7"));
-        Assertions.assertThrows(NumberFormatException.class, () -> Main.add("asd,zxc"));
 
-        Assertions.assertThrows(NumberFormatException.class, () -> Main.add("123123123123123123123123123,1"));
-        Assertions.assertThrows(NumberFormatException.class, () -> Main.add("-10000000000000, 03490349209"));
-        Assertions.assertThrows(NumberFormatException.class, () -> Main.add("0.00,1.5"));
-        Assertions.assertThrows(NumberFormatException.class, () -> Main.add("*&&^,!"));
+        Main addNumbers = new Main();
+
+        Assertions.assertThrows(NumberFormatException.class, () -> addNumbers.add(" ,7"));
+        Assertions.assertThrows(NumberFormatException.class, () -> addNumbers.add(" , "));
+        Assertions.assertThrows(NumberFormatException.class, () -> addNumbers.add("asd,7"));
+        Assertions.assertThrows(NumberFormatException.class, () -> addNumbers.add("asd,zxc"));
+
+        Assertions.assertThrows(NumberFormatException.class, () -> addNumbers.add("123123123123123123123123123,1"));
+        Assertions.assertThrows(NumberFormatException.class, () -> addNumbers.add("-10000000000000, 03490349209"));
+        Assertions.assertThrows(NumberFormatException.class, () -> addNumbers.add("0.00,1.5"));
+        Assertions.assertThrows(NumberFormatException.class, () -> addNumbers.add("*&&^,!"));
 
     }
 
